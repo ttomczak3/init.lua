d82f8523f725714d08c701088bfa681f7d9473c3
ttomczak <ttomczak1512@protonmail.com>
2023-04-22 01:16:15 -0500

    update

--- lua/ttomczak/packer.lua
+++ lua/ttomczak/packer.lua
@@ -50,7 +50,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
-        
+
     use "xiyaowong/nvim-transparent"
 
 end)
